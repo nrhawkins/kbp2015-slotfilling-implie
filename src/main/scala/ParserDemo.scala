1d1ffbd1e268bac7db4fae3f7ba72e0b5a922825
Gene Kim <genelkim03@gmail.com>
2014-11-07 11:49:07 -0800

    Made the parser demo into a tokenizer

--- src/main/scala/ParserDemo.scala
+++ src/main/scala/ParserDemo.scala
@@ -1,7 +1,7 @@
 
 import java.util.Collection
 import java.util.List
-import java.io.StringReader
+import java.io.{PrintWriter, StringReader}
 import edu.knowitall.tool.chunk.OpenNlpChunker
 import edu.stanford.nlp.process.Tokenizer
 import edu.stanford.nlp.process.TokenizerFactory
@@ -13,6 +13,8 @@ import edu.stanford.nlp.ling.HasWord
 import edu.stanford.nlp.ling.Sentence
 import edu.stanford.nlp.trees._
 import edu.stanford.nlp.parser.lexparser.LexicalizedParser
+
+import scala.io.Source
 
 object ParserDemo {
 
@@ -26,6 +28,10 @@ object ParserDemo {
    * TreePrint.printTree.
    */
   def main(args: Array[String]) {
+    val sentences = "testSet/tagger/sentences.txt"
+    val tokenFile = "testSet/tagger/tokenized.txt"
+    val lines = Source.fromFile(sentences).getLines()
+    val out = new PrintWriter(tokenFile)
     val sent2: String = "Oscar Emmanuel Peterson was born in the poor St. Antoine district of Montreal on Aug. 15, 1925, one of five children of Daniel Peterson, a West Indian immigrant, and the former Olivia John, whom Daniel had met in Montreal."
     /*val tokenizerFactory: TokenizerFactory[CoreLabel] = PTBTokenizer.factory(new CoreLabelTokenFactory, "")
     val tok: Tokenizer[CoreLabel] = tokenizerFactory.getTokenizer(new StringReader(sent2))
@@ -35,10 +41,15 @@ object ParserDemo {
       println(word.toString)
     }*/
     val chunker = new OpenNlpChunker()
-    val tokens = chunker.chunk(sent2)
-    for (token <- tokens) {
-      println(s"${token.string}-${token.offset}")
+    for (line <- lines) {
+      val tokens = chunker.chunk(line)
+      for (i <- 0 until tokens.size) {
+        out.print(s"${tokens(i).string}-${i+1} ")
+      }
+      out.println()
     }
+    out.close()
+    println(Source.fromFile(tokenFile).mkString)
   }
 }
 
