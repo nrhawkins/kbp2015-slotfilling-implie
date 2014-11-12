e6c87941975e074e57f3f5722c97409c5c98d446
Gene Kim <genelkim03@gmail.com>
2014-11-12 13:41:45 -0800

    Started writing relation extractor.

---
+++ src/main/scala/NounToNounRelation.scala
@@ -0,0 +1,5 @@
+/**
+ * Created by Gene on 11/10/2014.
+ */
+class NounToNounRelation(classTerm: String, relationType: String, modifiedNP: String, sentence: String = "") {
+}
--- src/main/scala/Relation.scala
+++
@@ -1,5 +0,0 @@
-/**
- * Created by Gene on 11/10/2014.
- */
-class Relation(classTerm: String, relationType: String, modifiedTerm: String) {
-}
