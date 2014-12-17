7f36b3a9bd64558b838e523bf891d37077cb0aac
Gene Kim <genelkim@cs.washington.edu>
2014-12-17 01:15:19 -0800

    Added test files

---
+++ get_random_files.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+# must be used from machine kupo.
+
+outdir=$(pwd)"/$1"
+
+cd "/projects/WebWare6/DEFT corpora/TAC_2010/TAC_2010_KBP_Source_Data/data/2009/nw"
+
+filename="random-1000-KBP.txt"
+while read line
+do
+    name=$line
+		cp $name $outdir
+done < "$filename"
