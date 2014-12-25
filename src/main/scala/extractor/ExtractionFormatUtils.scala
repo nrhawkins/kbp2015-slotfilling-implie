2eb24b4eda9581cbfdf3cbf4d2e88d3b13763108
Gene Kim <genelkim03@gmail.com>
2014-12-25 23:00:04 +0200

    Made a utils class holding some general extraction tools.

---
+++ src/main/scala/extractor/ExtractionFormatUtils.scala
@@ -0,0 +1,57 @@
+package extractor
+
+import edu.stanford.nlp.trees.TypedDependency
+
+import scala.collection.mutable
+
+/**
+ * Created by Gene on 12/24/2014.
+ */
+object ExtractionFormatUtils {
+  def verboseOutput(extractor: NounToNounRelationExtractor)(src: String) = {
+    // Parse tree + dependency list
+    val parse = extractor.getParse(src)
+    val builder = new mutable.StringBuilder
+    builder.append(parse._1.pennString())
+    for (td <- parse._2) {
+      builder.append(td + "\n")
+    }
+    builder.mkString
+  }
+
+  def extractionInfo(extractor: NounToNounRelationExtractor)(src: String): String = {
+    def printHops(map: Map[TagInfo, List[List[TypedDependency]]], builder: StringBuilder) {
+      for ((k, v) <- map) {
+        builder.append(s"Tag: ${k.text} has tag ${k.tag}\tDependency Hops:\t")
+        for (td <- v) {
+          builder.append(s"$td\t")
+        }
+        builder.append("\n")
+      }
+    }
+    val tags = extractor.getTags(src)
+    val (parse, tdl) = extractor.getParse(src)
+    val singleHops = extractor.dependencyHopsByTag(tags, tdl)
+    val singleGeneralized = singleHops map { case (k, v) => (k, v.map(td => td::Nil))}
+    val doubleHops = extractor.expandAllByOneHop(singleGeneralized, tdl)
+
+    val builder = mutable.StringBuilder.newBuilder
+    builder.append("Extraction Info\n")
+
+    for ((k, v) <- singleHops) {
+      builder.append(s"Tag: ${k.text} has tag ${k.tag}\tDependency Hops:\t")
+      builder.append("Single Hops ")
+      for (td <- v) {
+        builder.append(s"$td ")
+      }
+      builder.append("\t")
+      builder.append("Double Hops ")
+      for (td <- doubleHops.getOrElse(k, Nil)) {
+        builder.append(s"$td ")
+      }
+      builder.append("\n")
+    }
+
+    builder.mkString
+  }
+}
--- src/main/scala/testers/ExtractorTester.scala
+++ src/main/scala/testers/ExtractorTester.scala
@@ -4,7 +4,7 @@ import java.io.PrintWriter
 
 import com.typesafe.config.ConfigFactory
 import edu.stanford.nlp.trees.{TreePrint, TypedDependency}
-import extractor.{TagInfo, IndexedString, NounToNounRelation, NounToNounRelationExtractor}
+import extractor._
 import org.joda.time.DateTime
 
 import scala.collection.mutable
@@ -235,61 +235,14 @@ object ExtractorTester {
 
     if (showTagInfo) {
       // Print the relation hops for each tag.
-      out.append(extractionInfo(source))
+      out.append(ExtractionFormatUtils.extractionInfo(extractor)(source))
     }
 
     if (verbose) {
-      out.append(verboseOutput(source))
+      out.append(ExtractionFormatUtils.verboseOutput(extractor)(source))
     }
 
     (counter, out.mkString)
-  }
-
-  private def verboseOutput(src: String) = {
-    // Parse tree + dependency list
-    val parse = extractor.getParse(src)
-    val builder = new mutable.StringBuilder
-    builder.append(parse._1.pennString())
-    for (td <- parse._2) {
-      builder.append(td + "\n")
-    }
-    builder.mkString
-  }
-
-  private def extractionInfo(src: String): String = {
-    def printHops(map: Map[TagInfo, List[List[TypedDependency]]], builder: StringBuilder) {
-      for ((k, v) <- map) {
-        builder.append(s"Tag: ${k.text} has tag ${k.tag}\tDependency Hops:\t")
-        for (td <- v) {
-          builder.append(s"$td\t")
-        }
-        builder.append("\n")
-      }
-    }
-    val tags = extractor.getTags(src)
-    val (parse, tdl) = extractor.getParse(src)
-    val singleHops = extractor.dependencyHopsByTag(tags, tdl)
-    val singleGeneralized = singleHops map { case (k, v) => (k, v.map(td => td::Nil))}
-    val doubleHops = extractor.expandAllByOneHop(singleGeneralized, tdl)
-
-    val builder = mutable.StringBuilder.newBuilder
-    builder.append("Extraction Info\n")
-
-    for ((k, v) <- singleHops) {
-      builder.append(s"Tag: ${k.text} has tag ${k.tag}\tDependency Hops:\t")
-      builder.append("Single Hops ")
-      for (td <- v) {
-        builder.append(s"$td ")
-      }
-      builder.append("\t")
-      builder.append("Double Hops ")
-      for (td <- doubleHops.getOrElse(k, Nil)) {
-        builder.append(s"$td ")
-      }
-      builder.append("\n")
-    }
-
-    builder.mkString
   }
 
   /**
