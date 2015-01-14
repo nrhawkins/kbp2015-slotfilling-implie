f67fca329dae24da63ef57b2185eb6a68d6e2fef
Gene Kim <genelkim03@gmail.com>
2015-01-14 12:26:07 -0800

    Moved print functions.

--- src/main/scala/extractor/ImplicitRelationExtractor.scala
+++ src/main/scala/extractor/ImplicitRelationExtractor.scala
@@ -475,35 +475,4 @@ class ImplicitRelationExtractor(tagger: TaggerCollection[sentence.Sentence with 
   private def constructEnclosingPunctuation(punctConfs: List[Config]): List[EnclosingPunctuation] = {
     punctConfs.map(pc => EnclosingPunctuation(pc.getString("open"), pc.getString("close")))
   }
-
-  /**
-   * Printing functions (used for debugging).
-   */
-
-  // Prints a list of NounToNounRelations.
-  private def printNounToNounRelations(relns: Iterable[ImplicitRelation]) {
-    println("NounToNounRelations")
-    for (reln <- relns) {
-      println(reln)
-    }
-    println()
-  }
-
-  // Prints the labels of a tree.
-  private def printLabels(tree: Tree) {
-    println(s"${tree.label()}, ${tree.isLeaf}")
-    for (child <- tree.children()) {
-      printLabels(child)
-    }
-  }
-
-  // Prints details a of tdl.
-  private def printTdl(tdl: List[TypedDependency], message: String) {
-    println(message)
-    for (i <- 0 until tdl.size){
-      val td = tdl(i)
-      println(s"${td.dep()}, ${td.gov()}, ${td.reln().getShortName}, ${td.dep().tag()}")
-    }
-    println()
-  }
 }
