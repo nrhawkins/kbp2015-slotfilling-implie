35e7aef2063611a5583939598942dad9181a7eea
Natalie Hawkins <nrhawkins@gmail.com>
2015-01-26 14:27:06 -0800

    appended to answer key

---
+++ src/main/scala/tac/TACDevelopmentRelationExtractorForScoring.scala
@@ -0,0 +1,194 @@
+package tac
+
+import java.io.{FileWriter, BufferedWriter, PrintWriter}
+import java.nio.file.{Paths, Files}
+
+import com.typesafe.config.ConfigFactory
+import extractor.{ImplicitRelationExtractor, NERFilteredIRE, TaggerLoader}
+
+import scala.io.Source
+
+/**
+ * Main method takes the sentence file specified in tac-extractor.conf and
+ * outputs the extractions to a file.
+ *
+ * Results are tab-delimited for easy processing and reading from a spreadsheet
+ * program.
+ */
+object TACDevelopmentRelationExtractorForScoring {
+  case class InputLine(index: Int, docid: String, sentence: String)
+
+  val config = ConfigFactory.load("tac-extractor.conf")
+  val resultDir = config.getString("result-dir")
+  val sentenceDir = config.getString("sentence-dir")
+  val seqFilename = config.getString("sequence-file")
+
+  val sentenceFileSuffix = config.getString("sentence-file-suffix")
+  val resultFileSuffix = config.getString("result-file-suffix")
+  val nullFileSuffix = config.getString("null-result-file-suffix")
+
+  // Use the fact that the current sequence number should be 1 greater than
+  // the most recently generated sentence file.
+  val seq = getSeqNum(seqFilename) - 1
+
+  def main(args: Array[String]) {
+    // NOTE: very hacky arguments.
+    // Functionality changes depending on number of arguments.
+    val (inputLines, (out, nullout)) =
+      if (args.size > 1) {
+        // Run subset of sentences.
+        val sentenceMin = args(0).toInt
+        val sentenceMax = args(1).toInt
+        (subsetInput(sentenceMin, sentenceMax), outputs)
+      } else if (args.size > 0) {
+        // Restore/Continue run.
+        val sequenceNum = args(0).toInt
+        (incompleteInput(sequenceNum), appendOutputs(sequenceNum))
+      } else {
+        val result = (input, outputs)
+
+        // Starting a new file, print the column headers.
+        val columnHeaders = Array("Extraction Index", "Sentence Index", "DocId",
+          "Entity(NP)", "Relation", "Slotfill(tag)", "Sentence")
+        val nullColumnHeaders = Array("Extraction Index", "Sentence Index",
+          "DocId", "Sentence")
+        result._2._1.println(columnHeaders.tail.foldLeft
+                             (columnHeaders.head)
+                             ((acc, cur) => acc + s"\t$cur"))
+        result._2._2.println(nullColumnHeaders.tail.foldLeft
+                             (nullColumnHeaders.head)
+                             ((acc, cur) => acc + s"\t$cur"))
+        result
+      }
+
+    println("Loading Extractor.")
+    val relationExtractor =
+      new ImplicitRelationExtractor(
+        TaggerLoader.defaultTagger,
+        config.getString("tokenization-cache"),
+        config.getString("parse-cache"))
+
+    println("Extracting Sentences.")
+    println(s"${inputLines.size} sentences to process.")
+    var i1 = 0
+    var i2 = 0
+    for (inputLine <- inputLines) {
+      val extractions = relationExtractor.extractRelations(inputLine.sentence)
+      if (extractions.length != 0) {
+        for (extraction <- extractions) {
+          // Extraction Index, Sentence Index, Docid, Entity(NP), Relation, Slotfill(tag), Sentence
+          out.println(
+            s"$i1\t${inputLine.index}\t${inputLine.docid}" +
+              s"\t${extraction.np}\t${extraction.relation}\t${extraction.tag.asIndexedString}" +
+              s"\t${inputLine.sentence}")
+          i1 += 1
+        }
+      } else {
+        // If no extraction, write to null outputs.
+        nullout.println(
+          s"$i2\t${inputLine.index}\t${inputLine.docid}\t${inputLine.sentence}")
+        i2 += 1
+      }
+
+      if (inputLine.index % 100 == 0) {
+        println(s"Sentence ${inputLine.index} processed.")
+      }
+    }
+    out.close()
+    nullout.close()
+  }
+
+
+  def input = {
+    val inputFilename = sentenceDir + seq + sentenceFileSuffix
+
+    // Check if the file exists.
+    if (!Files.exists(Paths.get(inputFilename))) {
+      System.out.println(s"Sentence file $inputFilename doesn't exist!  " +
+        s"Please run the TACDevelopmentSentenceExtractor first, " +
+        s"or check that the sequence number in $seqFilename is correct.\n" +
+        s"Exiting...")
+      sys.exit(1)
+    }
+
+    Source.fromFile(inputFilename).getLines().map(line => {
+      val tokens = line.trim.split("\t")
+      InputLine(tokens(0).toInt, tokens(1), tokens(2))
+    }).toList
+  }
+
+  def incompleteInput(sequenceNum: Int) = {
+    val inputFilename = sentenceDir + sequenceNum + sentenceFileSuffix
+    val outFilename = resultDir + sequenceNum + resultFileSuffix
+
+
+    val lastSentenceProcessed = Source.fromFile(outFilename).getLines().toList.tail
+      .foldLeft(-1)((acc, cur) =>
+        Math.max(acc, cur.trim.split("\t")(1).toInt))
+
+    // Remove up to the last sentence processed since later lines may be incomplete.
+    val fixedFile = StringBuilder.newBuilder
+    val lines = Source.fromFile(outFilename).getLines().toList
+    fixedFile.append(lines.head + "\n")
+    var done = false
+    for (line <- lines.tail) {
+      if (line.trim.split("\t")(0).toInt < lastSentenceProcessed && !done) {
+        fixedFile.append(line)
+      } else {
+        done = true
+      }
+    }
+    new PrintWriter(outFilename).println(fixedFile.mkString)
+
+    // Get the input file.
+    Source.fromFile(inputFilename).getLines()
+      .filter(line => line.split("\t")(0).toInt >= lastSentenceProcessed)
+      .map(line => {
+      val tokens = line.trim.split("\t")
+      InputLine(tokens(0).toInt, tokens(1), tokens(2))
+    }).toList
+  }
+
+  def subsetInput(min: Int, max: Int) = {
+    val inputFilename = sentenceDir + seq + sentenceFileSuffix
+
+    // Get the input file.
+    Source.fromFile(inputFilename).getLines()
+    .map(line => line.trim.split("\t"))
+    .filter(tokens => {
+      val index = tokens(0).toInt
+      index >= min && index < max
+    })
+    .map(tokens => InputLine(tokens(0).toInt, tokens(1), tokens(2))).toList
+  }
+
+
+  def outputs = {
+    val outFilename = resultDir + seq + resultFileSuffix
+    val nullOutFilename = outFilename + nullFileSuffix
+
+    // Check if the file exists.
+    if (Files.exists(Paths.get(outFilename))) {
+      System.out.println(s"Result file $outFilename already exists!  " +
+        s"Please set the number in $seqFilename to a non conflicting " +
+        s"sequence number.\nExiting...")
+      sys.exit(1)
+    }
+
+    // If not, create new file with the given sequence num.
+    (new PrintWriter(outFilename), new PrintWriter(nullOutFilename))
+  }
+
+  def appendOutputs(sequenceNum: Int) = {
+    val outFilename = resultDir + sequenceNum + resultFileSuffix
+    val nullOutFilename = outFilename + nullFileSuffix
+    (new PrintWriter(new BufferedWriter(new FileWriter(outFilename, true))),
+      new PrintWriter(new BufferedWriter(new FileWriter(nullOutFilename, true))))
+  }
+
+  // This method is a bit of a hack.
+  // We know that the sentence num is incremented after the sentence extractor,
+  // so we take one less than what is recorded there.
+  def getSeqNum(sequenceFile: String) = Source.fromFile(sequenceFile)
+    .getLines().next().trim.toInt
+}
--- src/main/scala/testers/ExtractionScoring.scala
+++ src/main/scala/testers/ExtractionScoring.scala
@@ -24,10 +24,11 @@ object ExtractionScoring {
   // AnswerKeyItem fields:
   // 1)ExtractionIndex 2)SentenceIndex 3)DocId 4)Entity(NP) 5)Relation 
   // 6)Slotfill(tag) 7)Correct 8)Incorrect 9)Sentence
-  // *Don't need to keep all of the fields
+  // *Keep all of the fields so can re-write file with new extractions
   // ------------------------------------------------------------------------
-  case class AnswerKeyItem(sentIndex: Int, docid: String, relation: String, 
-      slotfill: String, correct: String, incorrect: String, sentence: String)
+  case class AnswerKeyItem(extrIndex: Int, sentIndex: Int, docid: String, 
+      entity: String, relation: String, slotfill: String, correct: String, 
+      incorrect: String, sentence: String)
   // ------------------------------------------------------------------------
   // ExtractionInputLine fields:
   // 1)ExtractionIndex 2)SentenceIndex 3)DocId 4)Entity(NP) 5)Relation 
@@ -184,38 +185,21 @@ object ExtractionScoring {
         val tokens = line.trim.split("\t")
           //println("tokens size: " + tokens.length)
           if(tokens.length ==9){
-            //sentIndex: Int, docid: String, relation: String, 
+            //extrIndex: Int, sentIndex: Int, docid: String, entity: String, relation: String, 
             //slotfill: String, correct: String, incorrect: String, sentence: String)
-            AnswerKeyItem(tokens(1).toInt, tokens(2), tokens(4), tokens(5), tokens(6),tokens(7), tokens(8))
+            AnswerKeyItem(tokens(0).toInt, tokens(1).toInt, tokens(2), tokens(3), tokens(4), 
+               tokens(5), tokens(6), tokens(7), tokens(8))
           }
           else{//tokens.length==7
-            AnswerKeyItem(tokens(1).toInt, tokens(2), tokens(4), tokens(5), "","", tokens(6))
+            AnswerKeyItem(tokens(0).toInt, tokens(1).toInt, tokens(2), tokens(3), tokens(4), tokens(5), 
+               "","", tokens(6))
           }
       }).toList
     }       
     
     println("AKI size: " + answerkeyItems.size)
     
-    println("Opening files for scoring report and new extractions")
-    
-    // -------------------------------------------------------
-    // Scoring Report - write out
-    //   1) the extractions and their score
-    //   2) summary of results, overall and by relation
-    //      -- # correct, # incorrect, precision
-    // -------------------------------------------------------  
-    val scoringreport = new PrintWriter(scoringreport_file)    
-    
-    // -------------------------------------------------------
-    // New Extractions - write out
-    //   1) the extractions not found in the answer key,
-    //        so they can be tagged
-    // -------------------------------------------------------  
-    val newextractions = new PrintWriter(newextractions_file)    
-    
-
     println("Building Answer Key")
- 
            
     // -------------------------------------------------------
     // Build Answer Key 
@@ -243,6 +227,59 @@ object ExtractionScoring {
 
     println("MK Correct size: " + matchkeyItemsCorrect.size)
     println("MK Incorrect size: " + matchkeyItemsIncorrect.size)
+    
+    
+    println("Opening files for scoring report and new extractions")
+
+    // -------------------------------------------------------
+    // -------------------------------------------------------
+    // Scoring Report - write out
+    //   1) the extractions and their score
+    //   2) summary of results, overall and by relation
+    //      -- # correct, # incorrect, precision
+    // -------------------------------------------------------
+    // -------------------------------------------------------
+    val scoringreport = new PrintWriter(scoringreport_file)    
+    
+    scoringreport.append("SentenceIndex" + "\t" + "DocumentId" + "\t" + 
+            "Entity" + "\t" + "Relation" + "\t" + "Slotfill(tag)" + 
+            "\t" + "Correct" + "\t" + "Incorrect" + "\t" + "Sentence" + "\n")               
+    
+    // -------------------------------------------------------        
+    // -------------------------------------------------------
+    // New Extractions - write out
+    //   1) the extractions not found in the answer key,
+    //        so they can be tagged
+    // -------------------------------------------------------  
+    // -------------------------------------------------------        
+    val newextractions = new PrintWriter(newextractions_file)    
+    //val newextractions = new PrintWriter(new BufferedWriter(new FileWriter(newextractions_file, true)))    
+
+    // -----------------------------------------------------------------------------
+    // Write existing Answer Key to a new file, 
+    //    here: write the header and existing answer key
+    //    further down in this file: append new extractions 
+    // -----------------------------------------------------------------------------       
+    
+    // Write header line
+    newextractions.append("ExtractionIndex" + "\t" + "SentenceIndex" + "\t" + "DocumentId" +
+        "\t" + "Entity" + "\t" + "Relation" + "\t" + "Slotfill" + "\t" + "Correct" + "\t" + 
+        "Incorrect" + "\t" + "Sentence" + "\n")
+
+    // Write correct answer key items
+    answerkeyItemsCorrect.foreach(k => {
+       newextractions.append(k.extrIndex + "\t" + k.sentIndex + "\t" + k.docid +
+           "\t" + k.entity + "\t" + k.relation + "\t" + k.slotfill + "\t" + 
+           k.correct + "\t" + k.incorrect + "\t" + k.sentence + "\n")             
+    })
+
+    // Write incorrect answer key items
+    answerkeyItemsIncorrect.foreach(k => {
+       newextractions.append(k.extrIndex + "\t" + k.sentIndex + "\t" + k.docid +
+           "\t" + k.entity + "\t" + k.relation + "\t" + k.slotfill + "\t" + 
+           k.correct + "\t" + k.incorrect + "\t" + k.sentence + "\n")             
+    })
+    
     
     println("Scoring Extractions")
     
@@ -358,9 +395,20 @@ object ExtractionScoring {
     // ------------------------------------------------
     // Write heading line for table
     // ------------------------------------------------
-    scoringreport.append("\n" + "Correct" + "\t\t" + "Error" + "\t\t" + 
-        "Precision" + "\t\t" + "Relation" + "\n\n")    
-
+    var xh = "Correct"
+    val formattedCorrectHeading = f"$xh%10s"
+    xh = "Incorrect"
+    val formattedIncorrectHeading = f"$xh%10s"
+    xh = "Precision"
+    val formattedPrecisionHeading = f"$xh%10s"
+    xh = "Relation"
+    val formattedRelationHeading = f"$xh%10s"
+    scoringreport.append("\n" + formattedCorrectHeading + "\t\t" + 
+        formattedIncorrectHeading + "\t\t" + 
+        formattedPrecisionHeading + "\t\t" + 
+        formattedRelationHeading + "\n")    
+    //val columnWidth = 10
+        
     // ------------------------------------------------    
     // Write results by relation    
     // ------------------------------------------------    
@@ -382,8 +430,15 @@ object ExtractionScoring {
       val countIncorrect = relationScoresIncorrect.getOrElse(extractionRelation, 0)
       val countMissed = matchkeyItemsCorrect.filter(mki => mki.relation == extractionRelation).size
       val testResultsRelation = new TestResults(countCorrect,countIncorrect,countMissed)
-      scoringreport.append(testResultsRelation.correct + "\t\t\t" + testResultsRelation.incorrect + 
-        "\t\t\t" + "%1.2f".format(testResultsRelation.precision) + "\t\t\t" + extractionRelation + "\n")  
+      var x = testResultsRelation.correct
+      val formattedCorrect = f"$x%10s"
+      x = testResultsRelation.incorrect
+      val formattedIncorrect = f"$x%10s"
+      var formattedPrecision = "%1.2f".format(testResultsRelation.precision)
+      formattedPrecision = f"$formattedPrecision%10s"
+      println(formattedCorrect)
+      scoringreport.append(formattedCorrect + "\t\t" + formattedIncorrect + "\t\t" + 
+           formattedPrecision + "\t\t" + extractionRelation + "\n")  
     })
         
         
@@ -394,8 +449,17 @@ object ExtractionScoring {
     testResults.incorrect = numIncorrect
     testResults.missed = numMissed    
     
-    scoringreport.append("\n" + testResults.correct + "\t\t\t" + testResults.incorrect + 
-        "\t\t\t" + "%1.2f".format(testResults.precision) + "\t\t\t" + "total" + "\n")    
+    var x = testResults.correct
+    val formattedCorrect = f"$x%10s"
+    x = testResults.incorrect
+    val formattedIncorrect = f"$x%10s"    
+    var formattedPrecision = "%1.2f".format(testResults.precision)
+    formattedPrecision = f"$formattedPrecision%10s"
+    
+    scoringreport.append("\n" + formattedCorrect + "\t\t" + 
+        formattedIncorrect + "\t\t" + 
+        formattedPrecision + "\t\t" 
+        + "total" + "\n")    
         
     
     println("Closing PrintWriters")    
