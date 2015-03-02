e45bd23e62456c26a57f0136964ce8be514e73f4
Gene Kim <genelkim@cs.washington.edu>
2015-03-02 08:50:20 -0800

    Changed experiment to check that basic filter functions work.

--- src/main/scala/experimenting/ExtractorTagTester.scala
+++ src/main/scala/experimenting/ExtractorTagTester.scala
@@ -1,6 +1,6 @@
 package experimenting
 
-import extractor.{FormalConstrainedImplIE, WordNetFilteredIRE, ImplicitRelationExtractor, TaggerLoader}
+import extractor._
 import testers.TaggerTester
 
 /**
@@ -10,23 +10,43 @@ object ExtractorTagTester {
   def main(args: Array[String]) {
 
     val tagger = TaggerLoader.defaultTagger
-    val extractor = new ImplicitRelationExtractor(tagger)
+//    val extractor = new ImplicitRelationExtractor(tagger)
+    val filtered = new ImplIEWithBasicFilters(tagger)
 /*
     val wordnetextractor = new WordNetFilteredIRE(tagger)
     val constrained = new FormalConstrainedImplIE(tagger)
 */
+
+      val sentences = List(
+          "John Arterberry, executive deputy chief of the fraud section in the Justice Department, said federal prosecutors and the FBI had made progress on mortgage fraud.",
+          "Staging a surprise detour from his trip to Sydney, Bush arrived at the Al-Asad air base in Anbar along with Secretary of State Condoleezza Rice and national security adviser Stephen Hadley.",
+          "AMSTERDAM, Netherlands 2008-01-26 21:41:45 UTC",
+          "In a recent speech to the Jewish Coalition, he went further, accusing the Democrats of putting too much stock in diplomacy.",
+          "Taipei, Sept. 11 (CNA) 09/11/07 20:10:07 (By T.C. Jiang)",
+          "SEOUL, South Korea 2007-02-16 03:13:47 UTC",
+          "Kyrgyzstan vs. Jordan",
+          "The client division sales \"are surprisingly ahead of where we thought they would come in,\" said Sid Parakh, an analyst at McAdams Wright Ragen.",
+          " According to the CWB forecast, Typhoon Hagupit is expected to move swiftly across the Bashi Channel and reach the waters southeast of Taiwan Monday, and that Taiwan's southeast region and the Kaohsiung-Pingtung area in southern Taiwan will begin to see rain from Monday afternoon, with the storm intensifying until Tuesday."
+      )
+
+      for (s <- sentences) {
+          println(s)
+          val results = filtered.extractRelations(s)
+          println(results)
+          println()
+      }
 //    val sentence = "John Arterberry, executive deputy chief of the fraud section in the Justice Department, said federal prosecutors and the FBI had made progress on mortgage fraud."
 //    val sentence = "Staging a surprise detour from his trip to Sydney, Bush arrived at the Al-Asad air base in Anbar along with Secretary of State Condoleezza Rice and national security adviser Stephen Hadley."
 
 //    val sentence = "AMSTERDAM, Netherlands 2008-01-26 21:41:45 UTC"
 //    val sentence = "In a recent speech to the Jewish Coalition, he went further, accusing the Democrats of putting too much stock in diplomacy."
 //    val sentence = "Taipei, Sept. 11 (CNA) 09/11/07 20:10:07 (By T.C. Jiang); SEOUL, South Korea 2007-02-16 03:13:47 UTC"
-    val sentence = "Kyrgyzstan vs. Jordan"
-    val tokens = extractor.getTokens(sentence)
+//    val sentence = "Kyrgyzstan vs. Jordan"
+//    val tokens = extractor.getTokens(sentence)
 
-    println(tokens)
-    println(tokens(0).chunkSymbol.name)
-    println(tokens(0).postagSymbol.name)
+//    println(tokens)
+//    println(tokens(0).chunkSymbol.name)
+//    println(tokens(0).postagSymbol.name)
 /*
     val constrainedresult = constrained.extractRelations(sentence)
     val wordnetresult = wordnetextractor.extractRelations(sentence)
