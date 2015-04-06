0709992a31acb27d19a6dc1cc64d94c04ff8e5d7
Gene Kim <genelkim@cs.washington.edu>
2015-04-05 17:40:47 -0700

    Experimenting with NER certainty.

--- src/main/scala/experimenting/NERTagger.scala
+++ src/main/scala/experimenting/NERTagger.scala
@@ -1,7 +1,8 @@
 package experimenting
 
-import edu.stanford.nlp.ie.crf.CRFClassifier
-import edu.stanford.nlp.util.CoreMap
+import edu.stanford.nlp.ie.crf.{CRFCliqueTree, CRFClassifier}
+import edu.stanford.nlp.ling.{CoreAnnotations, CoreLabel}
+import edu.stanford.nlp.util.{StringUtils, CoreMap}
 
 import scala.collection.JavaConversions._
 
@@ -11,10 +12,31 @@ import scala.collection.JavaConversions._
  */
 object NERTagger {
   def main(args: Array[String]): Unit = {
-    val classifier = CRFClassifier.DEFAULT_CLASSIFIER
-    println(classifier)
-/*
-    val results = classifier.classify("Hello.")
+
+    val classifier = CRFClassifier.getClassifier("models/english.all.3class.distsim.crf.ser.gz")
+//    val results = classifier.classify("Dr. Anthony Alessi, who co-chairs the sports neurology section of the American Academy of Neurology, said smaller studies had indicated repeated concussions or other brain injuries can bring on early dementia.")
+    val results = classifier.classify("Alessi was founded in 1921, and store shelves hold a history of familiar household items, such as the Dr. Skud fly swatter ($16) by French design superstar Philippe Starck.")
+
+    println(results)
+    println()
+
+    val cliqueTree = classifier.getCliqueTree(results(0))
+
+    for (i <- 0 until cliqueTree.length()) {
+      val wi = results(0).get(i)
+      val iter = classifier.classIndex.iterator()
+      while (iter.hasNext) {
+        val label = iter.next()
+        val index = classifier.classIndex.indexOf(label)
+        val prob = cliqueTree.prob(i, index)
+        System.out.println("\t" + label + "(" + prob + ")")
+      }
+      val tag = StringUtils.getNotNullString(wi.get(classOf[CoreAnnotations.AnswerAnnotation]))
+      println(wi)
+      System.out.println("Class : " + tag)
+    }
+
+
     for {
       result <- results.toList
     } {
@@ -22,6 +44,5 @@ object NERTagger {
         println(token)
       }
     }
-*/
   }
 }
