81d116b2ae05ba035120c5e5882c1a1c1bfbef34
Gene Kim <genelkim@cs.washington.edu>
2015-04-19 02:46:48 -0700

    Cleaned the long nationality list.

--- classTermLists/freebase/freebase_client.py
+++ classTermLists/freebase/freebase_client.py
@@ -17,9 +17,17 @@ api_key = "AIzaSyBfBytAi-_87cRCa3erhp-R4Uq6KwLQ2hk"
 service_url = 'https://www.googleapis.com/freebase/v1/mqlread'
 #query = [{'id': None, 'name': None, 'type': '/people/profession'}]
 #query = [{'id': None, 'name': None, 'collective_term_for_adherents': None, 'type': '/religion/religion'}]
-query = [{'id': None, 'name': None, 'type': '/location/citytown', \
+
+# Cities.
+#query = [{'id': None, 'name': None, 'type': '/location/citytown', \
+#  '/location/location/adjectival_form': [{'value': None, 'lang': '/lang/en', 'optional': True}],\
+#  '/common/topic/alias': [{'value': None, 'lang': '/lang/en', 'optional': True}]}]
+
+# Countries.
+query = [{'id': None, 'name': None, 'type': '/location/country', \
   '/location/location/adjectival_form': [{'value': None, 'lang': '/lang/en', 'optional': True}],\
   '/common/topic/alias': [{'value': None, 'lang': '/lang/en', 'optional': True}]}]
+
 params = {
 #  'query': json.dumps(query)
   'query': json.dumps(query),
@@ -37,9 +45,9 @@ for i in range(0, 50000):
   lst.extend([item['name'] for item in response['result'] if item['name'] is not None])
 
 
-  # City query.
-  lst.extend([adj['value'] for item in response['result'] for adj in item['/location/location/adjectival_form'] if adj['value'] is not None])
-  lst.extend([alias['value'] for item in response['result'] for alias in item['/common/topic/alias'] if alias['value'] is not None])
+  # City/Country query.
+  #lst.extend([adj['value'] for item in response['result'] for adj in item['/location/location/adjectival_form'] if adj['value'] is not None])
+  #lst.extend([alias['value'] for item in response['result'] for alias in item['/common/topic/alias'] if alias['value'] is not None])
 
   # Religion
   #lst.extend([profession['collective_term_for_adherents'] for profession in response['result'] if profession['collective_term_for_adherents'] is not None])
